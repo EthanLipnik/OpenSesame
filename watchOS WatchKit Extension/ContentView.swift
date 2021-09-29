6a5a260bea238927d8cbe9618dbce9835b89c84e
Ethan Lipnik <hi@ethanlipnik.com>
2021-09-29 16:56:20 -0400

    Added a watchOS target

---
+++ watchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchOS WatchKit Extension
+//
+//  Created by Ethan Lipnik on 9/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watchOS WatchKit Extension/OpenSesameApp.swift
@@ -0,0 +1,19 @@
+//
+//  OpenSesameApp.swift
+//  watchOS WatchKit Extension
+//
+//  Created by Ethan Lipnik on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct OpenSesameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
